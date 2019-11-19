3d22e50bb40734b4f7699fab0e9645393d5fee7d
mjtowarnicky+1@gmail.com <mjtowarnicky+1@gmail.com>
2019-11-18 19:42:00 -0700

    Initial commit

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+NewsArticle(props) {
+ <article>
+ <h1>{props.headline}</h1>
+ <img src={props.img}/>
+ <p></p>
+ </article>
+}
 
 export default NewsArticle;
