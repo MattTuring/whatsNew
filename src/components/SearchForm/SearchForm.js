0f2b77ed24fbbfa28b9cb11c328ae5ae225a6681
turing school <turingschool@turings-MacBook-Pro.local>
2019-12-02 13:57:46 -0700

    Removed document.qs to standard react pattern

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './SearchForm.css';
 
-const SearchForm = (props) => {
-  return (
-  <input onChange={() => props.search(document.querySelector('input').value)} placeholder='Search'></input>
-  )
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchValue: ''
+    }
+  }
+
+  updateSeachValue = (event) => {
+    this.setState({
+      searchValue: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <input onChange={(event) => {this.updateSeachValue(event); this.props.search(this.state.searchValue)}} placeholder='Search'></input>
+    )
+  }
 }
 
 export default SearchForm;
